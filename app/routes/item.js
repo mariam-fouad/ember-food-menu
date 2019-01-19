a897b0585f46b74c01298c995fc732e8068f15ba
Mariam <mariam.fouad94@gmail.com>
2019-01-19 23:00:04 +0300

    use dynamic data for menu item

--- app/routes/item.js
+++ app/routes/item.js
@@ -1,4 +1,8 @@
 import Route from '@ember/routing/route';
+import Ember from 'ember';
 
 export default Route.extend({
+    model(parms){
+        return Ember.$.get(`/menu/${parms.item_name}.json`);
+    }
 });
