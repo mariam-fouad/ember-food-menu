df10265a4cf372ddba36c0fc8b2d64a3cae1365e
Mariam <mariam.fouad94@gmail.com>
2019-01-20 19:18:03 +0300

    remove item from the food-tracher

--- app/components/order-tracker.js
+++ app/components/order-tracker.js
@@ -2,5 +2,10 @@ import Component from '@ember/component';
 import Ember from 'ember';
 
 export default Component.extend({
-    orderManger: Ember.inject.service('order-manger')
+    orderManger: Ember.inject.service('order-manger'),
+    actions:{
+        resetItem(day,meal){
+            this.get('orderManger').resetMenuMeal(day,meal);
+        }
+    }
 });
